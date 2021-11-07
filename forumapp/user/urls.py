014315006ae41d96c5cc18c82f9f27bdfd12b823
Thomas Glanzgit config --global user.email glanz.thomas.99@gmail.com <glanz.thomas.99@gmail.com>
2021-11-07 17:14:32 +0100

    sign up page + header

--- forumapp/user/urls.py
+++ forumapp/user/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from . import views
+from django.conf.urls import url
+#from mysite.core import views as core_views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('register', views.signup, name='signup')
 ]
 
 #test
