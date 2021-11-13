1a929d69511382c1278e72c1cadffc07f2a51b6b
Lorenz <lo.steidle@web.de>
2021-11-13 12:15:23 +0100

    Änderungen des Templates + Thread Beginning

--- forumapp/user/models.py
+++ forumapp/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    description = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
