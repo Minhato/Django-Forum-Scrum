b3914c06f5b78915cdb2a83498625c79ded3c2d5
Minh <Minh-Huynh@outlook.de>
2021-11-14 19:30:33 +0100

    register department hinzu

--- forumapp/user/forms.py
+++ forumapp/user/forms.py
@@ -4,10 +4,11 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    first_name = forms.CharField(max_length=30, required=True, help_text='required.')
+    last_name = forms.CharField(max_length=30, required=True, help_text='required.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    department = forms.CharField(max_length=30, required=True, help_text='Please enter your department.')
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name', 'email', 'department', 'password1', 'password2', )
