1f2ae66f4038a800dc52897878fe3dec1f72cc2e
Minh <Minh-Huynh@outlook.de>
2021-12-05 16:01:30 +0100

    test um user und thread app zu haben

---
+++ forumapp/thread/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from .models import Post
+
+
+
+class ThreadConfig(AppConfig):
+    name = 'thread'
--- forumapp/user/models.py
+++ forumapp/user/models.py
@@ -1,16 +1,2 @@
 from django.db import models
-from django.contrib.auth.models import User
-from tinymce.models import HTMLField
-
-class Post(models.Model):
-    title = models.CharField(max_length=20, unique=True)
-    description = models.CharField(max_length=80)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = HTMLField()
-    date = models.DateTimeField(auto_now_add=True)
-    date = models.DateTimeField(auto_now=True)
-    approved = models.BooleanField(default=False)
-    likes = models.ManyToManyField(User, related_name='forum_post_likes')
-    dislikes = models.ManyToManyField(User, related_name='forum_post_dislikes')
-    votes = models.IntegerField(default= 0)
 # Create your models here.
