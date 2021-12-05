209d8d39aab5c919309d298327f15f8859f56f22
Minh <Minh-Huynh@outlook.de>
2021-12-05 15:17:01 +0100

    forms änderung von thomas

--- forumapp/user/forms.py
+++ forumapp/user/forms.py
@@ -2,12 +2,18 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post #,Author
+from django.core.validators import RegexValidator
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-
+    letters = RegexValidator(r'^[a-zA-Z]+$', 'Only letter characters are allowed.')
+    # erweitert um die Felder und erbt von UsercreationForm, nötig da UserCreation form nur Username und Passwort enthält.
+    first_name = forms.CharField(max_length=30, required=True, help_text='required.', validators=[letters])
+    #first_name = forms.CharField(max_length=30, required=True,widget=forms.TextInput(attrs={'class':'form-control' , 'autocomplete': 'off','pattern':'[A-Za-z ]+', 'title':'Enter Characters Only '}))
+    last_name = forms.CharField(max_length=30, required=True, help_text='required.', validators=[letters]) 
+    email = forms.EmailField(max_length=254, help_text='required. Enter a valid email address.')
+    CHOICES = (('Option 1', 'IT'),('Option 2', 'Marketing'),('Option 3', 'Accounting'))
+    department = forms.ChoiceField(choices=CHOICES)
+    #department = forms.CharField(max_length=30, required=True, help_text='required')
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
@@ -16,8 +22,6 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content"]
-    
-
 
 class UpdateForm(forms.ModelForm):    
     class Meta:
